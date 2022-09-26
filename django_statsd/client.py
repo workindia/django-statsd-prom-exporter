f0555137cb8ae22a6020625b70108807df83b11b
Nikhil Kumar <nikhil.kumar@workindia.in>
2022-09-26 15:11:48 +0530

    Added support for multiple StatsD exporters
    - Modified expected configuration to add support for multiple exporters
    - Moved custom exception to separate module

--- django_statsd/client.py
+++ django_statsd/client.py
@@ -1,41 +1,74 @@
 from django.conf import settings
 from datadog import DogStatsd
+from django_statsd.exception import DjangoStatsdConfigurationMissingException
 
-_client = None
+"""
+# StatsD configuration for Django settings
+# ----------------------------------------
+STATSD_IGNORED_IPS = []
+STATSD_REQUEST_META_IP_PRECEDENCE_ORDER = (
+    'HTTP_X_ORIGINAL_FORWARDED_FOR',
+    'X_FORWARDED_FOR',
+    'HTTP_X_FORWARDED_FOR',
+    'HTTP_X_FORWARDED',
+    'HTTP_FORWARDED_FOR',
+    'REMOTE_ADDR'
+)
+STATSD_EXPORTERS = {
+    # Default Exporter
+    'default': {
+        'HOST': 'localhost',
+        'PORT': '9125',
+        'SERVICE_NAME': 'service-name',
+        'PREFIX': 'service_prefix'
+    },
+    # Secondary Exporter
+    'exporter_1': {
+        'HOST': 'statsd.host',
+        'PORT': '9125',
+        'SERVICE_NAME': 'service-name',
+        'PREFIX': 'service-prefix'
+    },
+}
+"""
+
+_clients = dict()
+_default_client = None
 _ignored_ips = None
 _request_meta_ip_precedence_order = None
-
-
-class DjangoStatsdConfigurationMissingException(Exception):
-
-    def __str__(self):
-        return 'STATSD_HOST, STATSD_PORT configuration is missing in django settings. ' \
-               'Django-Statsd cannot work without these settings.'
 
 
 def _get(key, default=None):
     return getattr(settings, key, default)
 
 
-def get_client():
-    host = _get('STATSD_HOST')
-    port = _get('STATSD_PORT')
-    service_name = _get('STATSD_SERVICE_NAME')
-    prefix = _get('STATSD_PREFIX', None)
+def get_client(exporter_alias: str = 'default'):
+    if exporter_alias in _clients:
+        return _clients[exporter_alias]
+
+    statsd_exporters: dict = _get('STATSD_EXPORTERS', {})
+    exporter_config = statsd_exporters.get(exporter_alias, {})
+
+    host = exporter_config.get('HOST')
+    port = exporter_config.get('PORT')
+    service_name = exporter_config.get('STATSD_SERVICE_NAME')
+    prefix = exporter_config.get('STATSD_PREFIX', None)
 
     if not host or not port:
-        raise DjangoStatsdConfigurationMissingException()
+        raise DjangoStatsdConfigurationMissingException(exporter_alias)
 
     constant_tags = []
 
     if service_name:
         constant_tags.append(f'service:{service_name}')
 
-    return DogStatsd(host=host, port=port, namespace=prefix, constant_tags=constant_tags)
+    client = DogStatsd(host=host, port=port, namespace=prefix, constant_tags=constant_tags)
+    _clients[exporter_alias] = client
+    return client
 
 
-if not _client:
-    _client = get_client()
+if not _default_client:
+    _default_client = get_client('default')
 
 if _ignored_ips is None:
     _ignored_ips = _get('STATSD_IGNORED_IPS', [])
@@ -50,6 +83,6 @@ if _request_meta_ip_precedence_order is None:
         'REMOTE_ADDR'
     ))
 
-client = _client
+client = _default_client
 IGNORED_IPS = _ignored_ips
 REQUEST_META_IP_PRECEDENCE_ORDER = _request_meta_ip_precedence_order
